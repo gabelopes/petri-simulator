2c463287d3e303ef2a5ecb4b38344a7fc54e5ae3
Gabriel Nunes <gabriel.nunes@sap.com>
2018-04-23 21:58:36 -0300

    Implements parallelism for transitions

--- common.pl
+++ common.pl
@@ -7,3 +7,10 @@ removeDuplicates([], []).
 removeDuplicates([H|T], L) :-
   removeDuplicates(T, S),
   (\+ member(H, S) -> L = [H|S]; L = S).
+
+
+do(0, _).
+do(T, G) :-
+  X is T - 1,
+  do(X, G),
+  G.
--- transitions.pl
+++ transitions.pl
@@ -3,10 +3,18 @@ isActive(T) :-
   forall(arc(P, transition(T), M), (hasEnoughMarks(P, M), isReady(P))),
   forall(inhibitoryArc(Q, transition(T), _), hasNoMarks(Q)).
 
+timesToTransition(P, M, T) :-
+  marks(P, X),
+  T is div(X, M).
+
+distributeMarks(T, P, M) :-
+  timesToTransition(P, M, X),
+  do(X, subtractMarks(P, M)),
+  forall(arc(transition(T), Q, N), do(X, addMarks(Q, N))).
+
 activate(T) :-
   isActive(T),
-  forall(arc(P, transition(T), M), subtractMarks(P, M)),
-  forall(arc(transition(T), Q, X), addMarks(Q, X)).
+  forall(arc(P, transition(T), M), distributeMarks(T, P, M)).
 
 isTransition(T) :- arc(_, transition(T), _).
 isTransition(T) :- arc(transition(T), _, _).
